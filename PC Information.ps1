09808876b3fc79d012425c698a655b26bc208a9d
Bartenbach <bbartenbach@chsomaha.org>
2016-05-18 14:04:52 -0500

    * Further conversion + added uptime and temperature functionality

--- PC Information.ps1
+++ PC Information.ps1
@@ -14,99 +14,79 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Write-Host "PC Information - Jonathon Ament, Blake Bartenbach - Copyright 2016" -foregroundcolor "green"
+Write-Host "PC Information - Jonathon Ament, Blake Bartenbach - Copyright 2016" -foregroundcolor "white"
 
-if (%os%==Windows_NT) {
-  echo "It works!"
-  Handle-WinNT
-} else {
-  Handle-Nocon
-}
-	
-function Handle-WinNT {
-  $system=
-  $manufacturer=
-  $model=
-  $serialnumber=
-  $biosversion=
-  $totalphysicalmemory=
-  $cpu=
-  $printers=
-  $osarchitecture=
-  $osname=
-  $sp=
-  $domain=
-  $username=
-  $cstring=
-  $ustring=
-  $pstring
-}
-
-function Get-PCName {
-  echo Please enter a PC Name or IP. !!YOU MUST HAVE ADMIN RIGHTS ON REMOTE PC!!
-  $computer=%computername%
-  set /p computer=[Enter a PC name or IP Address Here] 
-  echo ----------------
-}
-
-if (%computer% != %computername%) { 
-  Handle-Remote
-} else {
+function Handle-NoRPC {
+  Write-Host "----------------" -foregroundcolor "yellow"
+  Write-Host "Error...No connection could be made to [$computer]..." -foregroundcolor "red"
+  Write-Host "Error...Please try again..." -foregroundcolor "red"
+  Write-Host "----------------" -foregroundcolor "yellow"
+  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  clear
   Main
 }
 
-function Handle-Remote {
-  $cstring=/node:"%computer%"
+function Handle-BadUser {
+  Write-Host "----------------" -foregroundcolor "yellow"
+  Write-Host "Error...Access Denied using [%user%]..." -foregroundcolor "red"
+  Write-Host "Error...Please try again..." -foregroundcolor "red"
+  Write-Host "----------------" -foregroundcolor "yellow"
+  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  clear
+  Get-Username
 }
 
-function Get-Username {
-  # Get Username
-  # TO REENABLE username and password, REMOVE "goto start" BELOW THIS LINE.  Otherwise program assumes credentials of logged in individual.
-  goto start
-  #echo ----------------
-  #echo Type a Username and press Enter (!!MUST BE ADMIN ON REMOTE SYSTEM!!)
-  #set user=%username%
-  #set /p user=[Press Enter To Use Your Username - %username%]
-  #echo ----------------
+function Handle-Nocon {
+  Write-Host "----------------" -foregroundcolor "yellow"
+  Write-Host "Error...Invalid Operating System..." -foregroundcolor "red"
+  Write-Host "Error...No actions were made..." -foregroundcolor "red"
+  Write-Host "----------------" -foregroundcolor "yellow"
+  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  clear
+  Main
+}
 
-  # Check If Other Username
-  if (%user% != %username%) {
-    $ustring=/user:"%user%"
+function init {
+  $OS = Get-WmiObject -Computer $env:computername -Class Win32_OperatingSystem
+  if ($OS.caption -like "*Windows*") {
+    $system=
+    $manufacturer=
+    $model=
+    $serialnumber=
+    $biosversion=
+    $totalphysicalmemory=
+    $cpu=
+    $printers=
+    $osarchitecture=
+    $osname=
+    $sp=
+    $domain=
+    $username=
+    $cstring=
+    $ustring=
+    $pstring
+  } else {
+    Handle-Nocon
   }
-}
-
-function Get-Password {
-  echo ----------------
-  echo Type in a Password and press Enter (with or without DOMAIN)
-  $pass=
-  set /p pass=
-  echo ----------------
-}
-
-# Check if password was entered
-if ([%pass%]::IsNullOrEmpty) { 
-  $pstring=''
-} else {
-  $pstring=/password:"%pass%"
-  Main
 }
 
 function Main {
-  cls
-  echo Checking connection [Computer: %computer%]...
-  echo Please Wait....
+  init
+  $computer = Get-PCName
+  Write-Host "Checking connection [Computer: $computer ]..." -foregroundcolor "green"
+  Write-Host "Please Wait...." -foregroundcolor "green"
 
-  # Check connection
-  wmic %cstring% %ustring% %pstring% OS Get csname
+ # Check connection
+ # wmic $cstring $ustring $pstring OS Get csname
 
-  if (%errorlevel% == -2147023174) { 
-	Handle-NoRPC
-  } elseif (%errorlevel% == -2147024891) {
-	Handle-BadUser
-  }
+ # if (%errorlevel% == -2147023174) { 
+ #	 Handle-NoRPC
+ # } elseif (%errorlevel% == -2147024891) {
+ #   Handle-BadUser
+ # }
 
-  echo Getting data [Computer: %computer%]...
-  echo Please Wait....
+  Write-Host "Getting data [Computer: $computer]..." -foregroundcolor "green"
+  Write-Host "Please Wait...." -foregroundcolor "green"
 
   # Get Computer Name
   #FOR /F "tokens=2 delims='='" %%A in ('wmic %cstring% %ustring% %pstring% OS Get csname /value') do SET system=%%A
@@ -147,7 +127,7 @@ function Main {
 
   # Get Computer OS SP
   #FOR /F "tokens=2 delims='='" %%A in ('wmic %cstring% %ustring% %pstring% OS GET ServicePackMajorVersion /value') do SET sp=%%A
-  $sp = wmic $cstring $ustring $pstring os get servicepackmajoreversion /value
+  $sp = wmic $cstring $ustring $pstring os get servicepackmajorversion /value
 
   # Get Computer Username
   #FOR /F "tokens=2 delims='='" %%A in ('wmic %cstring% %ustring% %pstring% COMPUTERSYSTEM Get Username /value') do SET username=%%A
@@ -159,102 +139,140 @@ function Main {
 
   # Get Computer Memory in Gigabytes
   #FOR /F "usebackq tokens=1" %%A in (`powershell [Math]::round^(%totalphysicalmemory%/1024/1024/1024^)`) do SET memory=%%A
-  $memory = [Math]::round($totalphysicalmemory/1024/1024/1024)
+  #$memory = [Math]::round($totalphysicalmemory/1024/1024/1024)
   #TODO ^ combine
 
-  echo done!
+  Write-Host "done!" -foregroundcolor "green"
   clear
+  display
+}
+
+function Get-PCName {
+  Write-Host "Please enter a PC Name or IP. !!YOU MUST HAVE ADMIN RIGHTS ON REMOTE PC!!" -foregroundcolor "green"
+  $computer = Read-Host -prompt "[Enter a PC name or IP Address Here]"
+  Write-Host "----------------" -foregroundcolor "green"
+  
+  if ([string]::IsNullOrEmpty($computer)) {
+	return $env:computername
+  } else {
+	return "/node:" + $computer
+  }
+}
+
+function Get-Username {
+  # Get Username
+  # TO REENABLE username and password, REMOVE "goto start" BELOW THIS LINE.  Otherwise program assumes credentials of logged in individual.
+  Main
+  #echo ----------------
+  #echo Type a Username and press Enter (!!MUST BE ADMIN ON REMOTE SYSTEM!!)
+  #set user=%username%
+  #set /p user=[Press Enter To Use Your Username - %username%]
+  #echo ----------------
+
+  # Check If Other Username
+  if ($user -ne $env:username) {
+    $ustring="/user:" + $user
+  }
+}
+
+function Get-Temperature {
+  $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+  $currentTempKelvin = $t.CurrentTemperature / 10
+  $currentTempCelsius = $currentTempKelvin - 273.15
+  $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+  return $currentTempCelsius.ToString() + "°C"
+}
+
+function Get-Uptime {
+   $os = Get-WmiObject win32_operatingsystem
+   $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
+   $Display = "Uptime:               " `
+       + $Uptime.Days + " days, " + $Uptime.Hours + " hours, " + $Uptime.Minutes + " minutes" 
+   Write-Output $Display
+}
+
+#TODO this doesn't work yet
+function Get-Password {
+  Write-Host "----------------"
+  Write-Host "Type in a Password and press Enter (with or without DOMAIN)"
+  $pass=
+  set /p pass=
+  Write-Host "----------------"
+  
+  # Check if password was entered
+  if ($pass::IsNullOrEmpty) { 
+    $pstring=''
+  } else {
+    $pstring="/password:" + $pass
+  }
 }
 
 # Display everything on screen
 function display {
-  echo ------------------------------
-  echo COMPUTER:
-  echo System Name:      %system%
-  echo Manufacturer:     %manufacturer%
-  echo Model:            %model%
-  echo Serial Number:    %serialnumber%
-  echo BIOS Version:     %biosversion%
-  echo.
-  echo SPECS:
-  echo Total RAM:        %memory%GB
-  echo CPU:	          %cpu%
-  echo.
-  echo O/S:
-  echo Operating System: %osname%, %osarchitecture%
-  echo Service Pack:     %sp%
-  echo.
-  echo PRINTER(S):       %printers%
-  echo.
-  echo USER INFORMATION:
-  echo Domain\Username:  %username%
-  echo.
-  echo NETWORK PROPERTIES:
-  GETMAC /S %computer%
-  NSLOOKUP %computer%
-  echo ------------------------------
-  echo.
+  Write-Host "------------------------------"
+  Write-Host "COMPUTER:"
+  Write-Host "System Name:      $system"
+  Write-Host "Manufacturer:     $manufacturer"
+  Write-Host "Model:            $model"
+  Write-Host "Serial Number:    $serialnumber"
+  Write-Host "BIOS Version:     $biosversion"
+  Write-Host ""
+  Write-Host "SPECS:"
+  Write-Host "Total RAM:        $memory GB"
+  Write-Host "CPU:	          $cpu"
+  Write-Host ""
+  Write-Host "O/S:"
+  Write-Host "Operating System: $osname, $osarchitecture"
+  Write-Host "Service Pack:     $sp"
+  Write-Host ""
+  Write-Host "PRINTER(S):       $printers"
+  Write-Host ""
+  Write-Host "USER INFORMATION:"
+  Write-Host "Domain\Username:  $username"
+  Write-Host ""
+  Get-Uptime
+  Get-Temperature
+  Write-Host ""
+  Write-Host "NETWORK PROPERTIES:"
+  GETMAC /S $computer
+  NSLOOKUP $computer
+  Write-Host "------------------------------"
+  Write-Host ""
 }
 
 function generateFile {
-  SET file="%~dp0%computer%.txt"
-  echo ------------------------------ > %file%
-  echo COMPUTER: >>   %file%
-  echo System Name:   %system% >> %file%
-  echo Manufacturer:  %manufacturer% >> %file%
-  echo Model:         %model% >> %file%
-  echo Serial Number: %serialnumber% >> %file%
-  echo BIOS Version:  %biosversion% >> %file%
-  echo ------------------------------ >> %file%
-  echo ------------------------------ >> %file%
-  echo SPECS: >> %file%
-  echo Total RAM:     %totalphysicalmemory% >> %file%
-  echo CPU:	       %cpu% >> %file%
-  echo ------------------------------ >> %file%
-  echo ------------------------------ >> %file%
-  echo O/S: >> %file%
-  echo System:        %osname%, %osarchitecture% >> %file%
-  echo Service Pack:  %sp% >> %file%
-  echo Printers:      %printers% >> %file%
-  echo ------------------------------ >> %file%
-  echo ------------------------------ >> %file%
-  echo USER INFORMATION: >> %file%
-  echo Domain\User:   %username% >> %file%
-  echo ------------------------------ >> %file%
-  echo ------------------------------ >> %file%
-  echo NETWORK PROPERTIES: >> %file%
-  GETMAC /S %computer% >> %file%
-  NSLOOKUP %computer% >> %file%
-  echo ------------------------------ >> %file%
+  $file="%~dp0%computer%.txt"
+  echo ------------------------------ > $file
+  echo COMPUTER: >>   $file
+  echo System Name:   $system >> $file
+  echo Manufacturer:  $manufacturer >> $file
+  echo Model:         $model >> $file
+  echo Serial Number: $serialnumber >> $file
+  echo BIOS Version:  $biosversion >> $file
+  echo ------------------------------ >> $file
+  echo ------------------------------ >> $file
+  echo SPECS: >> $file
+  echo Total RAM:     $totalphysicalmemory >> $file
+  echo CPU:	       $cpu >> $file
+  echo ------------------------------ >> $file
+  echo ------------------------------ >> $file
+  echo O/S: >> $file
+  echo System:        $osname, $osarchitecture >> $file
+  echo Service Pack:  $sp >> $file
+  echo Printers:      $printers >> $file
+  echo ------------------------------ >> $file
+  echo ------------------------------ >> $file
+  echo USER INFORMATION: >> $file
+  echo Domain\User:   $username >> $file
+  echo ------------------------------ >> $file
+  echo ------------------------------ >> $file
+  echo NETWORK PROPERTIES: >> $file
+  GETMAC /S $computer >> $file
+  NSLOOKUP $computer >> $file
+  echo ------------------------------ >> $file
 
-  echo File created at %file%
+  Write-Host "File created at %file%"
   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 }
 
-function Handle-NoRPC {
-  echo ----------------
-  echo Error...No connection could be made to [%computer%]...
-  echo Error...Please try again...
-  echo ----------------
-  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-  cls
-  goto winnt
-}
-
-function Handle-BadUser {
-  echo ----------------
-  echo Error...Access Denied using [%user%]...
-  echo Error...Please try again...
-  echo ----------------
-  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-  cls
-  goto username
-}
-
-function Handle-Nocon {
-  echo ----------------
-  echo Error...Invalid Operating System...
-  echo Error...No actions were made...
-  echo ----------------
-  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-}
+Main
